11137ca690f74f749850d96d20754cfd48e0f929
William Owens <owensjrw@gmail.com>
2022-08-20 18:26:37 -0500

    Update sendmailwithpy.c

--- sendmailwithpy.c
+++ sendmailwithpy.c
@@ -1,13 +1,23 @@
 #include <stdlib.h>
 
-int main(void) {
-  system("python3 sendmail.py sendemail \"from@host\"        \
-                                        \"to@host\"          \
-                                        \"cc@host\"          \
-                                        \"subject\"          \
-                                        \"message.\"         \
-                                        \"smtpLogin\"        \
-                                        \"smtpPassword\"     \
-                                        \"smtpHost:port\"");
+int main(void)
+  char *from           = "someone@host",
+       *to             = "sentto@smsgateway",
+       *cc             = "",
+       *subject        = "Test",
+       *message        = "This is test message.",
+       *smtplogin      = "smtplogin",
+       *smtppassword   = "smtppassword",
+       *smtphostport   = "smtpserver:port";
+
+
+  system("python3 sendmail.py sendemail \""from               "\" \
+                                        \""to                 "\" \
+                                        \""cc                 "\" \
+                                        \""subject            "\" \
+                                        \""message            "\" \
+                                        \""smtplogin          "\" \
+                                        \""smtppassword       "\" \
+                                        \""smtphostport       "\"");
   return 0;
 }
