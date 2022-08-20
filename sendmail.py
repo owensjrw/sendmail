ff1dfa54af3ea43bb50efc35734e2f84ddf673dd
William Owens <owensjrw@gmail.com>
2022-08-20 18:01:46 -0500

    Add files via upload

---
+++ sendmail.py
@@ -0,0 +1,23 @@
+import sys
+import smtplib
+
+def sendemail(from_addr, to_addr_list, cc_addr_list,
+              subject, message,
+              login, password,
+              smtpserver):
+    message = message = f"""From: {from_addr}
+To: {','.join(to_addr_list)}
+Cc: {','.join(cc_addr_list)}
+Subject: {subject}
+
+{message}"""
+
+    server = smtplib.SMTP(smtpserver)
+    server.starttls()
+    server.login(login,password)
+    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.quit()
+    return problems
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]](*sys.argv[2:10])
---
+++ sendmailwithpy.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main(void) {
+  system("python3 sendmail.py sendemail \"from@host\"   \
+                                        \"to@host\"      \
+                                        \"cc@host\"                           \
+                                        \"subject\"                       \
+                                        \"message.\"    \
+                                        \"smtpLogin\"   \
+                                        \"smtpPassword\"                 \
+                                        \"smtpHost:port\"");
+  return 0;
+}
