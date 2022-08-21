4c6cff4ff978a7b12b3d1eb2a31921ed2c548aa7
William Owens <owensjrw@gmail.com>
2022-08-20 19:27:40 -0500

    Update sendmailwithpy.c

--- sendmailwithpy.c
+++ sendmailwithpy.c
@@ -1,23 +1,37 @@
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int main(void)
-  char *from           = "someone@host",
-       *to             = "sentto@smsgateway",
-       *cc             = "",
-       *subject        = "Test",
-       *message        = "This is test message.",
-       *smtplogin      = "smtplogin",
-       *smtppassword   = "smtppassword",
-       *smtphostport   = "smtpserver:port";
+int main(void) {
+  char *from           = "from@host",
+       *to             = "to@smsgateway",
+       *cc             = "cc@host",
+       *subject        = "Subject",
+       *message        = "Message",
+       *smtplogin      = "smtp lobin",
+       *smtppassword   = "smtp password",
+       *smtphostport   = "smtp server:port",
+       *command        = "python3 sendmail.py sendemail",
+       *sendmessage;
 
+  size_t msglen = strlen(from)           +
+                  strlen(to)             +
+                  strlen(cc)             +
+                  strlen(subject)        +
+                  strlen(message)        +
+                  strlen(smtplogin)      +
+                  strlen(smtppassword)   +
+                  strlen(smtphostport)   +
+                  strlen(command);
 
-  system("python3 sendmail.py sendemail \""from               "\" \
-                                        \""to                 "\" \
-                                        \""cc                 "\" \
-                                        \""subject            "\" \
-                                        \""message            "\" \
-                                        \""smtplogin          "\" \
-                                        \""smtppassword       "\" \
-                                        \""smtphostport       "\"");
+  sendmessage = malloc(msglen + 1);
+
+  sprintf(sendmessage, "%s \"%s\" \"%s\" \"%s\" \"%s\" "
+                       "\"%s\" \"%s\" \"%s\" \"%s\"",
+                        command, from, to, cc, subject, message,
+                        smtplogin, smtppassword, smtphostport);
+
+  system(sendmessage);
+
   return 0;
 }
