e9f147b9fd8a5b9cf557b0f8d3ef2b83cc88f1d4
William Owens <owensjrw@gmail.com>
2022-08-20 21:34:18 -0500

    Update sendmailwithpy.c

--- sendmailwithpy.c
+++ sendmailwithpy.c
@@ -1,37 +1,81 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+static void resetGetLine(char *inputBuffer, size_t *lenvar) {
+  inputBuffer = NULL;
+  lenvar = 0;
+}
+
+static void dropNewLine(char *getLineBuffer, ssize_t read){
+  getLineBuffer[read - 1] = '\0';
+}
+
 int main(void) {
-  char *from           = "from@host",
-       *to             = "to@smsgateway",
-       *cc             = "cc@host",
-       *subject        = "Subject",
-       *message        = "Message",
-       *smtplogin      = "smtp lobin",
-       *smtppassword   = "smtp password",
+  FILE *contactsFile;
+
+  char *from           = "user@domain",
+       *to,
+       *cc             = "",
+       *subject,
+       *message,
+       *smtplogin      = "smtp login",
+       *smtppassword   = "smtp password in plaintext",
        *smtphostport   = "smtp server:port",
        *command        = "python3 sendmail.py sendemail",
        *sendmessage;
 
-  size_t msglen = strlen(from)           +
-                  strlen(to)             +
-                  strlen(cc)             +
-                  strlen(subject)        +
-                  strlen(message)        +
-                  strlen(smtplogin)      +
-                  strlen(smtppassword)   +
-                  strlen(smtphostport)   +
-                  strlen(command);
+  size_t len = 0;
+  ssize_t read;
 
-  sendmessage = malloc(msglen + 1);
+  if ((contactsFile = fopen("contacts.txt", "r")) == NULL) {
+    fprintf(stderr, "Error - Unable to open contacts.txt");
+    exit(-1);
+  }
 
-  sprintf(sendmessage, "%s \"%s\" \"%s\" \"%s\" \"%s\" "
-                       "\"%s\" \"%s\" \"%s\" \"%s\"",
-                        command, from, to, cc, subject, message,
-                        smtplogin, smtppassword, smtphostport);
+  puts("Please enter the Subject:");
+  if ((read = getline(&subject, &len, stdin)) != -1) {
+    dropNewLine(subject, read);
+    len = 0;
+  } else {
+    fprintf(stderr, "Error - Unable to write Subject.");
+    exit(-2);
+  }
 
-  system(sendmessage);
+  puts("Please enter the message:");
+  if ((read = getline(&message, &len, stdin)) != -1) {
+    dropNewLine(message, read);
+    len = 0;
+  } else {
+    fprintf(stderr, "Error - Unable to write Message.");
+    exit(-3);
+  }
+
+  while ((read = getline(&to, &len, contactsFile)) != -1) {
+    dropNewLine(to, read);
+    size_t msglen = strlen(from)           +
+                    strlen(to)             +
+                    strlen(cc)             +
+                    strlen(subject)        +
+                    strlen(message)        +
+                    strlen(smtplogin)      +
+                    strlen(smtppassword)   +
+                    strlen(smtphostport)   +
+                    strlen(command);
+
+     sendmessage = malloc(msglen + 26);
+
+     sprintf(sendmessage, "%s \"%s\" \"%s\" \"%s\" \"%s\" "
+                          "\"%s\" \"%s\" \"%s\" \"%s\"\n",
+                          command, from, to, cc, subject, message,
+                          smtplogin, smtppassword, smtphostport);
+     resetGetLine(to, &len);
+     resetGetLine(subject, &len);
+     resetGetLine(message, &len);
+     //printf("%s", sendmessage);
+     system(sendmessage);
+   }
 
   return 0;
 }
